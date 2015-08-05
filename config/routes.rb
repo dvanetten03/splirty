3757455f68c9a7fd896abb6a3eccbaaa3865e696
Diane Van Etten <diane.vanetten@me.com>
2015-08-05 01:48:36 +0000

    Built a form to save new quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Splirty::Application.routes.draw do
 
   root 'quotes#index'
+
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
