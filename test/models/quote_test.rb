48c1d2ab28e86c708d5ec3420de9a854a8e13075
Diane Van Etten <diane.vanetten@me.com>
2015-08-05 20:07:50 +0000

    build a controller test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
 
-  test "unique_tag" do
-  	quote = Quote.create(:author => 'Diane Van Etten', :saying => 'Life is a journey, not a destination.')
-  	expected = 'DVE#' + quote.id.to_s
-  	actual = quote.unique_tag
-  	assert_equal expected, actual
-
+  test "unique_tag" do 
+    quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+    expected = 'KM#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
+  	
+
 end
